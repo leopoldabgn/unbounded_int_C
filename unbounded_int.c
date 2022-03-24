358a829919bc4eb085b9445a8607c2881d71e8b3
parismollo <parismollochristondis@gmail.com>
2022-03-24 21:50:14 +0100

    add: unbounded_int_somme_aux(unbounded_int a, unbounded_int b)
    - Function not ready. Requires final modifications.

--- unbounded_int.c
+++ unbounded_int.c
@@ -15,22 +15,25 @@ int main() {
     free(str);
     destroy_unbounded_int(u);
     */
-    unbounded_int u1 = string2unbounded_int("754");
-    unbounded_int u2 = string2unbounded_int("25");
+    unbounded_int u1 = string2unbounded_int("100");
+    unbounded_int u2 = string2unbounded_int("15");
     unbounded_int u3 = ll2unbounded_int(1000);
 
     printf("unbounded_int_cmp_unbounded_int(u1, u2) = %d\n", unbounded_int_cmp_unbounded_int(u1, u2));
     printf("unbounded_int_cmp_unbounded_int(u2, u1) = %d\n", unbounded_int_cmp_unbounded_int(u2, u1));
     unbounded_int sub = unbounded_int_difference(u1, u2);
+    // unbounded_int som = unbounded_int_somme_aux(u1, u2);
 
     // Je lis dans les deux sens pour verifier que tous
     // les liens precedent/suivant, premier/dernier fonctionne.
     print_unbounded_int(sub);
+    // print_unbounded_int(som);
     print_unbounded_int(u3);
 
     print_unbounded_int_left(sub);
 
     destroy_unbounded_int(sub);
+    // destroy_unbounded_int(som);
     destroy_unbounded_int(u1);
     destroy_unbounded_int(u2);
     destroy_unbounded_int(u3);
@@ -135,13 +138,14 @@ unbounded_int ll2unbounded_int(long long i) {
         @param i: integer  
         @return: integer represented in unbounded_int structure 
         This function converts a long long in a char*, then calls string2unbounded_int which returns a struct.
+
+        @see: snprintf(): A terminating null character is automatically appended after the content written.
     */
 
     int length = snprintf( NULL, 0, "%lld", i);
     char* str = malloc(length + 1);
 
     snprintf(str, length + 1, "%lld", i);
-    // str[length] = '\0'; INFO: A terminating null character is automatically appended after the content written.
     unbounded_int res = string2unbounded_int(str);
     free(str);
     return res;
@@ -153,7 +157,7 @@ int unbounded_int_cmp_unbounded_int(unbounded_int a, unbounded_int b) {
         @param: b -> integer represented by unbouded_int struct.
         @return: -1 if a < b; 0 if a == b; 1 if a > b
 
-        Comment (delete after reading): Je crois que si on pouvait faire  a - b ce serait plus simple, mais car
+        @see (delete after reading): Je crois que si on pouvait faire  a - b ce serait plus simple, mais car
         dans les exercices cela n'est pas encore fait en théorie, cette méthode de soustraction sera crée dans l'exo 7 donc
         je pense qu'il veut que je compare élément par élement au lieu de voir le résultat de a - b
     */
@@ -169,6 +173,70 @@ int unbounded_int_cmp_unbounded_int(unbounded_int a, unbounded_int b) {
         if(n_a->c < n_b->c) return -1;
     }
     return 0;
+}
+
+unbounded_int unbounded_int_somme_aux(unbounded_int a, unbounded_int b) {
+    /*
+        @consider: a, b > 0.
+        @todo: need to add lenght. 
+    */
+    unbounded_int error = (unbounded_int){.signe='*'};
+    if(a.signe == '*' || b.signe == '*')
+        return error;
+
+    unbounded_int sommeInt = {.premier=NULL, .dernier=NULL, .signe='+'};
+    int retenue = 0;
+    chiffre *c_n = NULL, *c_prev_n = NULL, *a_n=a.dernier, *b_n=b.dernier;
+
+    for(; a_n != NULL && b_n != NULL; a_n=a_n->precedent, b_n=b_n->precedent) {
+        int tmp = a_n->c + b_n->c + retenue;
+        retenue = tmp / 10;
+        c_n = malloc(sizeof(chiffre));
+        if(c_n == NULL) return error;
+        if(a_n == a.dernier || b_n == b.dernier) {sommeInt.dernier = c_n;}
+        c_n->c = tmp % 10;
+        c_n->suivant = c_prev_n;
+        if(c_prev_n != NULL) c_prev_n->suivant = c_n;
+        c_prev_n = c_n;
+    }
+
+    if (a.len > b.len) {
+        loop_and_add(a_n, retenue, c_n->precedent, error);
+    }
+    if (b.len > a.len) {
+        loop_and_add(b_n, retenue, c_n->precedent, error);
+    }
+
+    sommeInt.premier = c_n;
+    sommeInt.premier->precedent = NULL;
+    return sommeInt;
+}
+
+unbounded_int loop_and_add(chiffre* x_n, int retenue, chiffre* c_n, unbounded_int error) {
+    /*
+        @info: fonction auxiliaire, utilisé par unbounded_int_somme_aux(param1, param2)
+    */
+    chiffre* prev = x_n;
+    for(; x_n != NULL; x_n = x_n->precedent) {
+        int tmp = x_n->c + retenue;
+        retenue = tmp / 10;
+        c_n = malloc(sizeof(chiffre));
+        if(c_n == NULL) return error;
+        c_n->c = tmp % 10;
+        c_n->suivant = prev;
+        prev->suivant = c_n;
+        prev = c_n;
+    }
+
+    if(retenue != 0) {
+        c_n = malloc(sizeof(chiffre));
+        if(c_n == NULL) return error;
+        c_n->c = retenue;
+        c_n->suivant = prev;
+        prev->suivant = c_n;
+    }
+    unbounded_int x = {.premier=NULL, .dernier=NULL, .signe='+'};
+    return x;
 }
 
 char *unbounded_int2string(unbounded_int i) {
