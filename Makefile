5fb8310473c8fd54aae399b210254e5b7b36e577
parismollo <parismollochristondis@gmail.com>
2022-04-29 23:15:50 +0200

    update makefile

---
+++ Makefile
@@ -0,0 +1,85 @@
+# Instuction: Run make. That's it. 
+CC=gcc
+CFLAGS=-Wall -g -pedantic
+all: message clean calc_unbounded_int test_unbounded run_tests  run_calc
+$(VERBOSE).SILENT:
+
+message:
+	printf "\tWelcome to the Project C.\n"
+	sleep 1
+	printf "\t  by Léopold & Paris\n"
+	sleep 0.25
+	printf "[LOG]: Starting compiling"
+	sleep 0.25
+	printf "."
+	sleep 0.25
+	printf "."
+	sleep 0.25
+	printf "."
+	sleep 0.25
+	printf "Ready!\n"
+
+calc_unbounded_int: calc_unbounded_int.o unbounded_int.o
+	sleep 0.25
+	printf "[LOG]: Creating test_unbounded"
+	sleep 0.25
+	printf "."
+	sleep 0.25
+	printf "."
+	sleep 0.25
+	printf "."
+	$(CC) $(CFLAGS) -o $@ $^
+	sleep 0.25
+	printf "Ok!\n"
+
+calc_unbounded_int.o: calc_unbounded_int.c unbounded_int.h
+	$(CC) $(CFLAGS) -o $@ -c $<
+
+test_unbounded: test_unbounded.o unbounded_int.o
+	sleep 0.25
+	printf "[LOG]: Creating test_unbounded"
+	sleep 0.25
+	printf "."
+	sleep 0.25
+	printf "."
+	sleep 0.25
+	printf "."
+	$(CC) $(CFLAGS) -o $@ $^
+	sleep 0.25
+	printf "Ok!\n"
+
+test_unbounded.o: test_unbounded.c unbounded_int.h
+	$(CC) $(CFLAGS) -o $@ -c $<
+
+unbounded_int.o: unbounded_int.c unbounded_int.h
+	$(CC) $(CFLAGS) -o $@ -c $<
+
+run_tests:
+	sleep 1
+	./test_unbounded
+	printf "\n\n\n"
+
+run_calc:
+	printf "\t[LOG]: Running calculator"
+	sleep 0.25
+	printf "."
+	sleep 0.25
+	printf "."
+	sleep 0.25
+	printf ".\n"
+	./calc_unbounded_int
+
+clean:
+	sleep 0.25
+	printf "[LOG]: Cleaning files"
+	sleep 0.25
+	printf "."
+	sleep 0.25
+	printf "."
+	sleep 0.25
+	printf "."
+	rm -f *.o
+	rm -f test_unbounded
+	rm -f calc_unbounded_int
+	sleep 0.25
+	printf 'OK!\n'
--- test_unbounded.c
+++ test_unbounded.c
@@ -4,32 +4,41 @@
 #include "unbounded_int.h"
 
 int main() {
-    /*
-    unbounded_int u = string2unbounded_int("-22379308999827643656");
-    //printf("%ld\n", u.len);
-    //printf("%c\n", u.premier->c);
-    print_unbounded_int(u);
-    char* str = unbounded_int2string(u);
-    printf("%s\n", str);
-    free(str);
-    destroy_unbounded_int(u);
-    */
+    printf("\n");
+    printf("\t\t [LOG]: Running test_unbounded.c\n\n");
     unbounded_int u1 = string2unbounded_int("500045421444");
     unbounded_int u2 = string2unbounded_int("-325154544");
     unbounded_int u3 = ll2unbounded_int(1000);
 
-    printf("unbounded_int_cmp_unbounded_int(u1, u2) = %d\n", unbounded_int_cmp_unbounded_int(u1, u2));
+
+    printf("[LOG]:Test #1\n");
+
+    printf("unbounded_int u1 = string2unbounded_int(500045421444) --> "); print_unbounded_int(u1);
+    printf("unbounded_int u2 = string2unbounded_int(-325154544) --> "); print_unbounded_int(u2);
+    printf("unbounded_int u3 = ll2unbounded_int(1000) --> "); print_unbounded_int(u3);
+
+
+    printf("\n");
+
+    printf("[LOG]:Test #2");
+
+    printf("\nunbounded_int_cmp_unbounded_int(u1, u2) = %d\n", unbounded_int_cmp_unbounded_int(u1, u2));
     printf("unbounded_int_cmp_unbounded_int(u2, u1) = %d\n", unbounded_int_cmp_unbounded_int(u2, u1));
 
     char* u1_str = unbounded_int2string(u1);
     long long test = 911;
+
+    printf("\n");
+    printf("[LOG]: Test #3\n");
     printf("unbounded_cmp between unbounded_int %s and long long %lld = %d\n",
            u1_str, test, unbounded_int_cmp_ll(u1, test));
     free(u1_str);
 
     printf("\n");
-    print_unbounded_int(u1);
-    print_unbounded_int(u2);
+
+    printf("[LOG]:Test #4\n");
+    printf("u1 = "); print_unbounded_int(u1);
+    printf("u2 = "); print_unbounded_int(u2);
     
     unbounded_int sub = unbounded_int_difference(u1, u2);
     unbounded_int som = unbounded_int_somme(u1, u2);
@@ -37,18 +46,15 @@ int main() {
 
     // Je lis dans les deux sens pour verifier que tous
     // les liens precedent/suivant, premier/dernier fonctionne.
-    printf("Soustraction: ");
-    print_unbounded_int(sub);
-    print_unbounded_int(u3);
-    printf("Somme: ");
-    print_unbounded_int(som);
-    print_unbounded_int_left(sub);
-    print_unbounded_int_left(som);
+    printf("Soustraction: "); print_unbounded_int(sub);
+    // print_unbounded_int(u3);
+    printf("Somme: "); print_unbounded_int(som);
+    // print_unbounded_int_left(sub);
+    // print_unbounded_int_left(som);
 
-    printf("Produit: \n");
+    
     unbounded_int prod = unbounded_int_produit(u1, u2);
-    print_unbounded_int(prod);
-
+    printf("Produit: "); print_unbounded_int(prod);
 
     destroy_unbounded_int(sub);
     destroy_unbounded_int(som);
@@ -56,7 +62,7 @@ int main() {
     destroy_unbounded_int(u2);
     destroy_unbounded_int(u3);
     destroy_unbounded_int(prod);
-
-
+    printf("\n");
+    printf("\t\t[LOG]: Finished");
     return EXIT_SUCCESS;
 }
