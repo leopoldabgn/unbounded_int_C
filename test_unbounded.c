1b3699576f9c354c8fbc73e04c73bb527d9f8282
Leopold Abignoli <leopold.abignoli@gmail.com>
2022-05-21 17:07:23 +0200

    Ajout d'une fonction pour faire des tests facilement

--- test_unbounded.c
+++ test_unbounded.c
@@ -3,9 +3,66 @@
 
 #include "unbounded_int.h"
 
+static int cmp(int a, int b) {
+    if(a < b)
+        return -1;
+    return a > b;
+}
+
+static int calc(int a, char op, int b) {
+    switch(op) {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        default:
+            return -1;
+    }
+}
+
+static void test_unbounded_operations() {
+    int val1[] = {-1, 0, 1, 10, -10, 600, 999, -9999};
+    int val2[] = {6, 120, -12, 20, -16, -100, 1, -1};
+    char ops[] = {'+', '-', '*'};
+
+    int s1 = sizeof(val1) / sizeof(val1[0]);
+    int s2 = sizeof(ops) / sizeof(ops[0]);
+
+    unbounded_int a, b, res;
+
+    for(int i=0;i<s1;i++) {
+        printf("[LOG] Test #%d :\n\n", i+1);
+        a = ll2unbounded_int(val1[i]);
+        b = ll2unbounded_int(val2[i]);
+
+        printf("COMPARAISONS : %d et %d\n\n", val1[i], val2[i]);
+        printf("unbounded_int : %d\n", unbounded_int_cmp_unbounded_int(a, b));
+        printf("          int : %d\n\n", cmp(val1[i], val2[i]));
+
+        for(int j=0;j<s2;j++) {
+            printf("CALCUL : %d %c %d\n", val1[i], ops[j], val2[i]);
+
+            res = calculate(a, ops[j], b);
+            printf("\nunbounded_int : ");
+            print_unbounded_int(res);
+            printf("          int : %d\n\n", calc(val1[i], ops[j], val2[i]));
+
+            destroy_unbounded_int(res);
+        }
+
+        destroy_unbounded_int(a);
+        destroy_unbounded_int(b);
+    }
+
+}
+
 int main() {
     printf("\n");
     printf("\t\t [LOG]: Running test_unbounded.c\n\n");
+    test_unbounded_operations();
+    
     unbounded_int u1 = string2unbounded_int("500045421444");
     unbounded_int u2 = string2unbounded_int("-325154544");
     unbounded_int u3 = ll2unbounded_int(1000);
@@ -64,5 +121,6 @@ int main() {
     destroy_unbounded_int(prod);
     printf("\n");
     printf("\t\t[LOG]: Finished");
+
     return EXIT_SUCCESS;
 }
